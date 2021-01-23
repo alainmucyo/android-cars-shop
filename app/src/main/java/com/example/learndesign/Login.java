f1aa942edea85bc16fd4d51a0611ea65a75dd049
Alain MUCYO <alainmucyo3@gmail.com>
2021-01-23 20:53:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/learndesign/Login.java
@@ -0,0 +1,21 @@
+package com.example.learndesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+    }
+}
